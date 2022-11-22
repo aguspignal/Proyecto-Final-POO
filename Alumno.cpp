fa8a097b0176fde4b3d778ab740c0873d768538f
gasparrrx6 <gasparpelayo44@gmail.com>
2022-11-22 10:53:24 -0300

    Cambios en atributos y  constructor
    Alumno no debe estar compuesto por un Curso, Alumno es parte de un Curso

--- Alumno.cpp
+++ Alumno.cpp
@@ -3,6 +3,4 @@
 
 Alumno::Alumno(string t_nombre, string t_passowrd, string t_email, int t_dni,
                int t_edad, int t_anioIngreso, int t_anio, char t_division)
-    : Usuario(t_nombre, t_passowrd, t_email, t_dni, t_edad, t_anioIngreso) {
-  m_curso(t_anio, t_division);
-}
+    : Usuario(t_nombre, t_passowrd, t_email, t_dni, t_edad, t_anioIngreso){};
--- Alumno.hpp
+++ Alumno.hpp
@@ -7,15 +7,6 @@
 using namespace std;
 
 class Alumno : public Usuario {
-private:
-  Curso m_curso;
-  string m_nombre;
-  string m_passowrd;
-  string m_email;
-  int m_dni;
-  int m_edad;
-  int m_anio;
-
 public:
   Alumno(string t_nombre, string t_passowrd, string t_email, int t_dni,
          int t_edad, int t_anioIngreso, int t_anio, char t_division);
