bca011cbe22968ffbdad6566ee1f8850255b0fbc
gasparrrx6 <gasparpelayo44@gmail.com>
2022-11-22 01:28:44 -0300

    Push total
    Porque hice add . sin querer

--- Curso.cpp
+++ Curso.cpp
@@ -1,34 +1,32 @@
-#include "Curso.h"
+#include "Curso.hpp"
 #include <iostream>
 
-/// Establecer curso con un año y division;
-Curso::Curso(int anio, char division){
-	this->curso = anio;
-	this->division = division;
+/// Establecer curso con un aï¿½o y division;
+Curso::Curso(int anio, char division) {
+  this->curso = anio;
+  this->division = division;
 }
 
-/// Retornar año y division
-string Curso::getCurso(){
-	string str = (to_string(curso) + " " + division);
-	return str;
+// Constructor por defecto en primer anio
+Curso::Curso() {
+  this->curso = 1;
+  this->division = 'A';
 }
 
-/// Añadir nuevo ALumno/Docente/Preceptor al curso
-void Curso::addIntegrante(Usuario u){
-	integrantes.push_back(u);
+/// Retornar aï¿½o y division
+string Curso::getCurso() {
+  string str = (to_string(curso) + " " + division);
+  return str;
 }
 
-/// Retornar vector con Alumnos, Docentes y Precetores 
-vector<Usuario> Curso::getIntegrantes(){
-	return integrantes;
-}
+/// Aï¿½adir nuevo ALumno/Docente/Preceptor al curso
+void Curso::addIntegrante(Usuario u) { integrantes.push_back(u); }
 
-/// Añadir materia al curso
-void Curso::addMateria (string m){
-	materias.push_back(m);
-}
+/// Retornar vector con Alumnos, Docentes y Precetores
+vector<Usuario> Curso::getIntegrantes() { return integrantes; }
+
+/// Aï¿½adir materia al curso
+void Curso::addMateria(string m) { materias.push_back(m); }
 
 /// Retornar vector con Materias
-vector<string> Curso::getMaterias(){
-	return materias;
-}
+vector<string> Curso::getMaterias() { return materias; }
