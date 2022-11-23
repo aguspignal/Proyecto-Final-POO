d9d00bbd94caff7d263f96d7bcc2876cfad489a6
gasparrrx6 <gasparpelayo44@gmail.com>
2022-11-22 22:42:55 -0300

    Class Alumno terminada
    Nuevos metodos para cargar y ver las notas de un alumno

--- Alumno.hpp
+++ Alumno.hpp
@@ -4,12 +4,19 @@
 #include "Curso.hpp"
 #include "Usuario.hpp"
 #include <iostream>
+#include <map>
 using namespace std;
 
 class Alumno : public Usuario {
+private:
+  map<string, float> m_historiaAcademica;
+  int d = 0;
+
 public:
   Alumno(string t_nombre, string t_passowrd, string t_email, int t_dni,
          int t_edad, int t_anioIngreso, int t_anio, char t_division);
+  void setNota(string t_materia, float t_nota);
+  float getNota(string t_materia);
 };
 
 #endif
