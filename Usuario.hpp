e5991de18830ca93201bba85944d14c8c9eca0de
gasparrrx6 <gasparpelayo44@gmail.com>
2022-12-08 18:24:18 -0300

    Acomodando cosas

--- Usuario.hpp
+++ Usuario.hpp
@@ -1,29 +1,30 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
-#include<string>
 #include <cstring>
+#include <string>
+
 using namespace std;
 
 class Usuario {
-	char nombre[60];
-	char password[20];
-	char email[50];
-	int dni;
-	int edad;
-	int anio_ingreso;
-	
+  char m_nombre[60];
+  char m_password[20];
+  char m_email[50];
+  int m_dni;
+  int m_edad;
+  int m_anioIngreso;
+
 public:
-	Usuario(string nombre, string passowrd, string email, int dni, int edad, int anio);
-	
-	string getNombre();
-	string getPassword();
-	string getEmail();
-	int getDNI();
-	int getEdad();
-	int getIngreso();
-	
-	void changePassword(string psw);
+  Usuario(string t_nombre, string t_passowrd, string t_email, int t_dni,
+          int t_edad, int t_anio);
+  string getNombre();
+  string getPassword();
+  string getEmail();
+  int getDNI();
+  int getEdad();
+  int getIngreso();
+
+  void changePassword(string t_psw);
 };
 
 #endif
